24c829f3bcd945c6242bd2d2cb2bd7fda707c9f4
ageev <ageev@SERGO>
2025-02-03 15:03:43 +0200

    Add project files.

---
+++ Cinema-webapp/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cinema_webapp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
