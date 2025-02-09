6cf015f4a885565384a48ab8d46014c291fa38ab
alexeyprvz <alexeyprvz@knu.ua>
2025-02-10 00:03:06 +0200

    Create model Movie and Hall

---
+++ Cinema-webapp/Models/Hall.cs
@@ -0,0 +1,10 @@
+﻿namespace Cinema_webapp.Models
+{
+    public class Hall
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Seat> Seats { get; set; }
+        public List<Showtime> Showtimes { get; set; }
+    }
+}
---
+++ Cinema-webapp/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿namespace Cinema_webapp.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Genre { get; set; }
+        public int DurationMinutes { get; set; }
+        public string PosterUrl { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public float Rating { get; set; }
+        public List<Showtime> Showtimes { get; set; }
+    }
+}
