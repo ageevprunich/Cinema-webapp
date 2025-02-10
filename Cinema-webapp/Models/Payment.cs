a7e0bf0962740c8f502bdae122d9f9e6160144d3
DenisRudiy <denryd2311@gmail.com>
2025-02-10 10:48:08 +0200

    Create model Ticket and Payment

---
+++ Cinema-webapp/Models/Payment.cs
@@ -0,0 +1,13 @@
+﻿namespace Cinema_webapp.Models
+{
+    public class Payment
+    {
+        public int Id { get; set; }
+        public int TicketId { get; set; }
+        public Ticket Ticket { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public string PaymentMethod { get; set; } // "Credit Card", "PayPal", "Cash"
+        public decimal Amount { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ Cinema-webapp/Models/Ticket.cs
@@ -0,0 +1,14 @@
+﻿namespace Cinema_webapp.Models
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public int ShowtimeId { get; set; }
+        public Showtime Showtime { get; set; }
+        public int UserId { get; set; }
+        public int SeatId { get; set; }
+        public Seat Seat { get; set; }
+        public decimal Price { get; set; }
+        public string Status { get; set; }
+    }
+}
