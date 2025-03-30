2437c1a1a88260e22a0641f3fbcf570db187053a
DenisRudiy <denryd2311@gmail.com>
2025-03-31 01:46:09 +0300

    Create Views pages

--- Cinema-webapp/Controllers/MovieController.cs
+++ Cinema-webapp/Controllers/MovieController.cs
@@ -1,24 +1,37 @@
-﻿using Cinema_webapp.Data;
-using Cinema_webapp.Models;
+﻿//using Cinema_webapp.Data;
+//using Cinema_webapp.Models;
+//using Microsoft.AspNetCore.Mvc;
+
+//namespace Cinema_webapp.Controllers
+//{
+//    public class MovieController : Controller
+//    {
+//        private readonly ApplicationDbContext _db;
+
+//        public MovieController(ApplicationDbContext db)
+//        {
+//            _db = db;
+//        }
+
+//        [HttpGet]
+//        public IActionResult Index()
+//        {
+//            List<Movie> movie = _db.Movies.ToList();
+
+//            return View(movie);
+//        }
+//    }
+//}
+
 using Microsoft.AspNetCore.Mvc;
 
 namespace Cinema_webapp.Controllers
 {
     public class MovieController : Controller
     {
-        private readonly ApplicationDbContext _db;
-
-        public MovieController(ApplicationDbContext db)
-        {
-            _db = db;
-        }
-
-        [HttpGet]
         public IActionResult Index()
         {
-            List<Movie> movie = _db.Movies.ToList();
-
-            return View(movie);
+            return View();
         }
     }
 }
--- Cinema-webapp/Models/Ticket.cs
+++ Cinema-webapp/Models/Ticket.cs
@@ -8,7 +8,6 @@
         public decimal Price { get; set; }
         public string Status { get; set; }
         public int? PaymentId { get; set; }
-
         public Showtime Showtime { get; set; }
         public Seat Seat { get; set; }
         public Payment Payment { get; set; }
