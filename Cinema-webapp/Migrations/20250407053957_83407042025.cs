4af15428fe5ef305a2151f75243bdfbc0df59d21
Serhii <ageev.prunich@gmail.com>
2025-04-07 09:16:41 +0300

    Add style

---
+++ Cinema-webapp/Migrations/20250407053957_83407042025.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cinema_webapp.Migrations
+{
+    /// <inheritdoc />
+    public partial class _83407042025 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
